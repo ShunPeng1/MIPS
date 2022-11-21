7468f1b9740c524acabe779213c8770cef1ef655
ShunPeng1 <shunpeng1@gmail.com>
2022-11-21 11:00:19 +0700

    Update connnect4.asm

--- connnect4.asm
+++ connnect4.asm
@@ -1,34 +1,24 @@
 .data 
 	
 	board:		.asciiz "    _      ___     ____    _ _     ___      __     ____  \n   / |    |_  )   |__ /   | | |   | __|    / /    |__  | \n   | |     / /     |_ \\   |_  _|  |__ \\   / _ \\     / /  \n   |_|    /___|   |___/     |_|   |___/   \\___/    /_/   \n                                                         \n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|       |       |       |       |       |       |       |\n|_______|_______|_______|_______|_______|_______|_______|\n"
-	buffer:		.space 1000
-
+	# String board is row and column size is 58 . 44
 	
+	buffer:		.space 1000
 	
 	welcome: 	.asciiz "               WELCOME TO                        \n  ______    ______   __    __  __    __  ________   ______  ________        __    __ \n /      \\  /      \\ |  \\  |  \\|  \\  |  \\|        \\ /      \\|        \\      |  \\  |  \\\n|  $$$$$$\\|  $$$$$$\\| $$\\ | $$| $$\\ | $$| $$$$$$$$|  $$$$$$\\\\$$$$$$$$      | $$  | $$\n| $$   \\$$| $$  | $$| $$$\\| $$| $$$\\| $$| $$__    | $$   \\$$  | $$         | $$__| $$\n| $$      | $$  | $$| $$$$\\ $$| $$$$\\ $$| $$  \\   | $$        | $$         | $$    $$\n| $$   __ | $$  | $$| $$\\$$ $$| $$\\$$ $$| $$$$$   | $$   __   | $$          \\$$$$$$$$\n| $$__/  \\| $$__/ $$| $$ \\$$$$| $$ \\$$$$| $$_____ | $$__/  \\  | $$               | $$\n \\$$    $$ \\$$    $$| $$  \\$$$| $$  \\$$$| $$     \\ \\$$    $$  | $$               | $$\n  \\$$$$$$   \\$$$$$$  \\$$   \\$$ \\$$   \\$$ \\$$$$$$$$  \\$$$$$$    \\$$                \\$$\n\n                                            by BANH TAN THUAN - 2153011\n"
 	menu_choice: 	.asciiz "\n\n   .---------------.              .---------------. \n  | .-------------. |            | .-------------. |\n  | |  1. Start   | |            | |  2. Guide   | |\n  | '-------------' |            | '-------------' |\n   '---------------'              '---------------' \n\n"
-	menu_error_1_2:	.asciiz "\nThe choice you've entered is not in the specified range (1-2). \nPlease enter the right choice: "
+	menu_error_1_2:	.asciiz "\nTHE CHOICE YOU'VE ENTERED IS NOT IN THE SPECIFIED RANGE (1-2). PLEASE ENTER THE RIGHT CHOICE: "
 
+	player_1_turn:	.asciiz "PLAYER 1 TURN, PLEASE SELECT THE ROW (1-7) : "
+	player_2_turn:	.asciiz "PLAYER 2 TURN, PLEASE SELECT THE ROW (1-7) : "
+        turn_error_1_7:	.asciiz "\nTHE CHOICE YOU'VE ENTERED IS NOT IN THE SPECIFIED RANGE (1-7). PLEASE ENTER THE RIGHT CHOICE: "
 	
-	x1:		.asciiz "|  \\ /  |"
-	x2:		.asciiz "|   X   |"
-	x3:		.asciiz "|  / \\  |"
 	
-	
-	e0:		.asciiz "|       |"
-	e1:		.asciiz "|_______|"	
-
 	
 		
-	o1:		.asciiz "|   _   |"
-	o2:		.asciiz "|  /O\\  |"
-	o3:		.asciiz "|  \\_/  |"
-
+	tutorial_text:	.asciiz "tutorialText\n"
 	
-		
-	tutorialText:	.asciiz "tutorialText\n"
-	
-	new_page:        .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+	new_page:        .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 	endl:	.asciiz "\n"
 	
 	
@@ -90,9 +80,13 @@ tutorial:
 	
 game_loop:
 	
-	j print_board
-	 
+	jal print_board
+	
+	
+	   
 	j quit 
+	
+
 	
 flush_console: # Use to push all printed messages out of the console space.
 	li $v0, 4
@@ -101,7 +95,10 @@ flush_console: # Use to push all printed messages out of the console space.
 	jr $ra
 	
 
-print_board: # loop through 6 row first, with 4 sub row in each row
+print_board: # 
+	addi $sp , $sp. $-4
+	sw $ra, 0($sp)
+	
 	jal flush_console
 	
 	li $v0, 4
@@ -117,7 +114,10 @@ print_board: # loop through 6 row first, with 4 sub row in each row
 	la $a0,board
 	syscall
 	
-	j quit
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
 
 calculate_coordinate:  # a2 = row_index(1-6) , a3 = column index (1-7)  return v1 = board middle coordinate 
 	addi $a2, $a2, -1
@@ -152,30 +152,32 @@ insert_x:  #  a2 = row_index(1-6) , a3 = column index (1-7) . Void : Find middle
 	sb $t0, 56($v1)
 	sb $t0, 60($v1)
 	
-	#return
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
 	
 	jr $ra
 	
-insert_o:  #  a2 = row_index(1-6) , a3 = column index (1-7)
-	#find middle position of the index
+insert_o:  #  a2 = row_index(1-6) , a3 = column index (1-7) . Void : Find middle position of the index, then replace with text shape X
+	
 	
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 	
 	jal calculate_coordinate
 	
+	# t0 = 88 is ascii for 'zero'
 	addi $t0, $0, 48
 	add $v1, $v1, $s7
-	
-	sb $t0, 0($v1)
-	sb $t0, 1($v1)
-	sb $t0, -1($v1)
-	sb $t0, -60($v1)
-	sb $t0, -56($v1)
-	sb $t0, 56($v1)
-	sb $t0, 60($v1)
+
+	#draw 0 shape		
+	sb $t0, -59($v1)
+	sb $t0, -58($v1)
+	sb $t0, -57($v1)
+	sb $t0, 2($v1)
+	sb $t0, -2($v1)
+	sb $t0, 59($v1)
+	sb $t0, 58($v1)
+	sb $t0, 57($v1)
 	
 
 	lw $ra, 0($sp)
